7fc606186678d0d87814f8c9dd0bf02df686760f
federicofusco <federico.fusco@overslight.com>
2022-05-10 15:05:31 +0200

    Organized author fallback components

--- ecomentality/components/profile/ProfileFallback.jsx
+++ ecomentality/components/profile/ProfileFallback.jsx
@@ -1,22 +1,35 @@
 import Button from "../buttons/Button"
 import ProfileImage from "./ProfileImage"
+import Head from "next/head"
+import GenericNavbar from "./../nav/GenericNavbar"
 
 const ProfileFallback = () => {
 	return (
-		<div className="mx-6 p-4 border border-gray-300 rounded-lg flex justify-between">
-			<div className="flex w-full">
-				<div className="w-16">
-					<ProfileImage isFallback={ true } />
-				</div>
-				<div className="my-auto w-full ml-6 max-w-xs">
-					<div className="animate-pulse bg-gray-300 rounded-lg w-full text-transparent h-3">a</div>
-					<div className="animate-pulse mt-1 bg-gray-300 rounded-lg w-3/4 text-transparent h-3">a</div>
+
+		<>
+			<Head>
+				<title>Loading - GEM</title>
+				<meta name="language" content="EN" />
+				<meta name="robots" content="index, follow" />
+			</Head>
+			<GenericNavbar />
+			<div className="mt-24">
+			<div className="mx-6 p-4 border border-gray-300 rounded-lg flex justify-between">
+				<div className="flex w-full">
+						<div className="w-16">
+							<ProfileImage isFallback={ true } />
+						</div>
+						<div className="my-auto w-full ml-6 max-w-xs">
+							<div className="animate-pulse bg-gray-300 rounded-lg w-full text-transparent h-3">a</div>
+							<div className="animate-pulse mt-1 bg-gray-300 rounded-lg w-3/4 text-transparent h-3">a</div>
+						</div>
+					</div>
+					<div className="hidden sm:block max-h-20 my-auto">
+						<Button disabled={ true } rounded={ false }>Random article</Button>
+					</div> 
 				</div>
 			</div>
-			<div className="hidden sm:block max-h-20 my-auto">
-				<Button disabled={ true } rounded={ false }>Random article</Button>
-			</div> 
-		</div>
+		</>
 	)
 }
 
