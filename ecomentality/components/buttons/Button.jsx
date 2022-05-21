b7758875bfdc6d75dc84ce646419e91d6636d6fd
federicofusco <federico.fusco@overslight.com>
2022-05-21 15:24:32 +0200

    Added graphics to image modal

--- ecomentality/components/buttons/Button.jsx
+++ ecomentality/components/buttons/Button.jsx
@@ -7,11 +7,11 @@
  * @param {?Boolean} disabled - Whether or not the button should be disabled (default: false)
  * @returns A button
  */
-const Button = ({ onClick, children, rounded = true, disabled = false }) => {
+const Button = ({ className, onClick, children, rounded = true, disabled = false }) => {
 
 	const click = () => !disabled ? onClick () : null;
 
-	return <button onClick={ click } className={`${ disabled ? "text-gray-200 bg-green-800 cursor-not-allowed" : "" } text-white font-medium bg-green-600 ${ rounded ? "rounded-full" : "rounded-md" } py-1 px-3 text-sm ml-auto`}>
+	return <button onClick={ click } className={`${ className } ${ disabled ? "text-gray-200 bg-green-800 cursor-not-allowed" : "" } text-white font-medium bg-green-600 ${ rounded ? "rounded-full" : "rounded-md" } py-1 px-3 text-sm ml-auto`}>
 		{ children }
 	</button>
 }
--- ecomentality/components/forms/modal/EditorImageModal.jsx
+++ ecomentality/components/forms/modal/EditorImageModal.jsx
@@ -13,10 +13,13 @@ const EditorImageModal = ({ visible, onClick }) => {
 
 	return (
 		<div className="w-screen h-screen top-0 left-0 absolute">
-			<div className="absolute p-4 h-full sm:h-80 w-full sm:w-3/4 max-w-md border border-gray-300 rounded-none sm:rounded-md bg-white text-black transform left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+			<div className="absolute p-4 h-full sm:h-auto w-full sm:w-3/4 max-w-md border border-gray-300 rounded-none sm:rounded-md bg-white text-black transform left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
 				<h1>Insert image</h1>
-				<input type="url" placeholder="https://www.example.com/image.png" ref={ linkRef } />
-				<Button onClick={ click }>Insert</Button>
+
+				<div className="w-full flex justify-start">
+					<input className="w-full border h-8 border-gray-300 rounded-l-md px-2 py-1 mt-3 " type="url" placeholder="https://www.example.com/image.png" ref={ linkRef } />
+					<Button className="rounded-l-none h-8 mt-3 rounded-r-md py-1 px-2" onClick={ click }>Insert</Button>
+				</div>
 			</div>
 		</div>
 	)
