37760db9fb03de1a300ae9fa606cccc92c50d4e0
federicofusco <federico.fusco@overslight.com>
2022-05-21 15:07:54 +0200

    Fixed lint warning

--- ecomentality/components/editor/EditorImage.jsx
+++ ecomentality/components/editor/EditorImage.jsx
@@ -1,17 +1,19 @@
 import DeleteImageButton from "../buttons/DeleteImageButton"
+import Image from "next/image"
 
 /**
  * Displays an image in the editor
  * 
- * @param {Boolean} hideDeleteButton - Whether or not to hide the delete button
+ * @param {Boolean} hideDeleteButton - Whether or not to hide the delete button 
  * @returns An image
  */
 const ArticleImage = ({ attributes, element, children, hideDeleteButton }) => {
 
 	return (
-		<div {...attributes} contentEditable={ false } className="relative select-none flex justify-center">
-			<img 
+		<div {...attributes} contentEditable={ false } className="relative object-contain select-none flex justify-center">
+			<Image 
 				src={ element.src }
+				layout="fill"
 				alt=""
 				className="block max-w-full h-full max-h-80 select-none" />
 			
