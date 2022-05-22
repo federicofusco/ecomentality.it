123ec51409d78e4472b78de17c6b23e68fa2255c
federicofusco <federico.fusco@overslight.com>
2022-05-22 20:59:46 +0200

    Videos are now called interviews, fixed graphical bug in interview editor

--- ecomentality/components/buttons/icon/editor/DeleteImageButton.jsx
+++ ecomentality/components/buttons/icon/editor/DeleteImageButton.jsx
@@ -25,13 +25,6 @@ const DeleteImageButton = ({ element }) => {
 			<MdDelete className="m-auto" />
 		</GenericIconButton>
 	)
-
-	// return <button
-	// 	onClick={() => Transforms.removeNodes ( editor, { at: path })}
-	// 	title="Delete image"
-	// 	className={`${ selected && focused ? "inline" : "none" } h-7 w-7 absolute top-2 left-2 bg-white border-black border rounded-full`}>
-	// 	<MdDelete className="m-auto" />
-	// </button>
 }
 
 export default DeleteImageButton;
