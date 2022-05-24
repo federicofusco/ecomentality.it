1423e2e9fbc43fdd0a6bfd690bf96df74233181b
federicofusco <federico.fusco@overslight.com>
2022-05-24 23:34:36 +0200

    Added real-time like count on video page

--- ecomentality/components/buttons/icon/video/LikeButton.jsx
+++ ecomentality/components/buttons/icon/video/LikeButton.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import useVideo from "./../../../../hooks/video"
 import { useSnackbar } from "notistack"
 
@@ -8,17 +8,25 @@ import GenericActionButton from "./../GenericActionButton"
 /**
  * A button which likes a video
  * 
- * @param {Number} likeCount - The video's like count
  * @param {String} id - The video's ID
  * @param {?Boolean} isFallback - Whether or not to display a fallback button (default: false)
  * @note There is no grafical difference if isFallback is true, it just won't attempt to like the video
  * @returns A button for liking an video
  */
-const VideoLikeButton = ({ likeCount, id, isFallback = false }) => {
+const VideoLikeButton = ({ id, isFallback = false }) => {
 
 	const [liked, setLiked] = useState ( false );
-	const { likeVideo, dislikeVideo } = useVideo ();
+	const [likeCount, setLikeCount] = useState ( "..." );
+	const { likeVideo, dislikeVideo, fetchLikeCount, createLikeListener } = useVideo ();
 	const { enqueueSnackbar } = useSnackbar ();
+
+	const unsub = createLikeListener ( id, ( doc ) => {
+
+		if ( !doc.exists () ) return;
+
+		// Updates the like count
+		setLikeCount ( doc.data ().likeCount );
+	});
 
 	const onClick = () => {
 
@@ -46,12 +54,37 @@ const VideoLikeButton = ({ likeCount, id, isFallback = false }) => {
 		setLiked ( !liked );
 	}
 
+	useEffect (() => {
+
+		// Updates the like count
+		const updateLikeCount = async () => {
+			await fetchLikeCount ( id ) 
+				.then (( likeCount ) => {
+
+					// Updates the like count
+					setLikeCount ( likeCount.data.likeCount );
+				})
+				.catch (( error ) => {
+
+					console.error ( error );
+
+					// An error occurred
+					enqueueSnackbar ( "Something went wrong!", {
+						variant: "error",
+						autoHideDuration: 3000
+					});
+				});
+		}
+
+		updateLikeCount ();
+	}, []);
+
 	return (
 		<GenericActionButton
 			onClick={ onClick }
 			className={ liked ? "text-green-600" : "text-gray-600" }>
 			{ liked ? <MdThumbUp /> : <MdThumbUpOffAlt /> }
-			<p className="text-xs">{ liked ? likeCount + 1 : likeCount }</p>
+			<p className="text-xs">{ likeCount }</p>
 		</GenericActionButton>
 	)
 }
