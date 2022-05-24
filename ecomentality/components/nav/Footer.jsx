dd6481be90e28b0e104549c431e1f74b3c7f14fe
federicofusco <federico.fusco@overslight.com>
2022-05-24 18:51:08 +0200

    Fixed footer links

--- ecomentality/components/nav/Footer.jsx
+++ ecomentality/components/nav/Footer.jsx
@@ -7,22 +7,18 @@ const LinkList = () => {
 			<div>
 				<h3 className="text-white font-black font-poppins uppercase text-sm">Link e contatti</h3>
 				<div className="mt-2">
-					<Link  href={'mailto:gem@ecomentality.life'}>
+					<Link  href="mailto:gem@ecomentality.life">
 						<a className="block  text-white w-14 mt-1">gem@ecomentality.life</a>
 					</Link>
-					<Link  href={'www.liceocalini.edu.it'}>
+					
+					<Link  href="https://www.liceocalini.edu.it">
 						<a className="block  text-white w-36 mt-1">Liceo A. Calini</a>
 					</Link>
+
 					<span className="block text-white w-45 mt-1">Via Monte Suello 2 - Brescia</span>
-					<Link  href={'tel:gem@ecomentality.life'}>
+					<Link  href="tel:gem@ecomentality.life">
 						<a className="block  text-white w-36 mt-1">+39 030 390249</a>
 					</Link>
-					
-					{/* { ["1", "2", "3", "4"].map ( link => (
-						<Link key={ link } href={`/${ link }`}>
-							<a className="block text-white w-14 mt-1">Link { link }</a>
-						</Link>
-					))} */}
 
 				</div>
 			</div>
@@ -36,28 +32,10 @@ const Copyright = () => {
 			<div>
 				<h3 className="text-white font-black font-poppins uppercase text-sm ">Extra</h3>
 				<div className="mt-2">
-					<Link  href={'www.ecomentality.life'}>
+					<Link  href="/">
 						<a className="block  text-white w-14 mt-1">www.ecomentality.life</a>
 					</Link>
 					<span className="block text-white w-45 mt-1">&copy; Great Eco Mentality 2022</span>
-				</div>
-			</div>
-		</div>
-	)
-}
-const MemberList = () => {
-	return (
-		<div className="w-1/2 sm:w-full flex justify-center sm:justify-start sm:pl-8 mt-4">
-			<div>
-				<h3 className="text-white font-black font-poppins uppercase text-sm">Our team</h3>
-				<div className="mt-2">
-
-					{ ["1", "2", "3", "4"].map ( link => (
-						<Link key={ link } href={`/${ link }`}>
-							<a className="block text-white w-auto mt-1">Member { link }</a>
-						</Link>
-					))}
-
 				</div>
 			</div>
 		</div>
@@ -69,10 +47,10 @@ const SocialList = () => {
 		<div >
 				<h3 className="uppercase my-auto font-poppins font-black text-white mt-4">Find Us</h3>
 				<div className="text-white w-20 flex  mt-1">
-					<Link  href={'https://www.instagram.com/gemliceocalini/'}>
+					<Link  href="https://www.instagram.com/gemliceocalini/">
 						<FaInstagram className="ml-0 text-xl my-auto" />	
 					</Link>
-					<Link  href={'https://www.facebook.com/profile.php?id=100081198311689/'}>
+					<Link  href="https://www.facebook.com/profile.php?id=100081198311689/">
 						<FaFacebook className="ml-3 text-xl my-auto" />
 						
 					</Link>
@@ -84,7 +62,7 @@ const SocialList = () => {
 
 const Footer = () => {
 	return (
-		<footer className="bg-dark-green w-screen p-8 h-auto sm:flex sm:justify-center " style={{justifyContent: 'space-evenly'}}>
+		<footer className="bg-dark-green w-screen p-8 h-auto sm:flex sm:justify-evenly">
 			<LinkList />
 			<Copyright />
 			<SocialList />
